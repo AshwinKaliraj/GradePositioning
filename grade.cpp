263edc8ec1fc6b775983b9237d0afbb90e7e85fe
AshwinKaliraj <ashwin.kaliraj@psiog.com>
2025-08-17 02:15:57 +0530

    2

--- grade.cpp
+++ grade.cpp
@@ -6,3 +6,19 @@ int initialization();
 bool process(int score);
 void grade(int score);
 void terminal();
+
+int initialization() {
+    int score;
+    cout << "Enter your Score: ";
+    cin >> score;
+    return score;
+}
+
+bool process(int score) {
+    if (score >= 0 && score <= 100) {
+        return true; 
+    } else {
+        cout << "Invalid score!" << endl;
+        return false; 
+    }
+}
